495614cbabc9b79d1d5a83dac1ac937d58f2ca34
ClubinhoTech <clubelulli@gmail.com>
2026-08-23 20:43:34 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "lele-v11";
+const CACHE_NAME = "lele-v12";
 
 const APP_FILES = [
   "./",
   "./index.html",
-  "./styles.css?v=11",
-  "./app.js?v=11",
+  "./styles.css?v=12",
+  "./app.js?v=12",
   "./manifest.webmanifest"
 ];
 
